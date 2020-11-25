24f3e0cbcfde41d7266e4ff040df4212e6f721ea
Devng Langhanoja <62923975+mrdebu5@users.noreply.github.com>
2020-11-25 12:39:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Navbar from "./components/Navbar";
+import "./App.css";
+// import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+}
+
+export default App;
